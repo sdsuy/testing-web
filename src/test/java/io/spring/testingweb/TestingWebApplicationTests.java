fe1b80fed7b1b5722fb6a700574dc16d79ae69f7
Santiago Da Silva <sdasilva@outlook.es>
2020-12-11 01:18:39 -0300

    first commit

---
+++ src/test/java/io/spring/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
