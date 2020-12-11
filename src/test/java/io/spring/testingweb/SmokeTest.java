c02d95ff3568e9d5d95805e2ccf6fd67c8e3b87f
Santiago Da Silva <sdasilva@outlook.es>
2020-12-11 02:30:46 -0300

    Controller creation

---
+++ src/test/java/io/spring/testingweb/SmokeTest.java
@@ -0,0 +1,21 @@
+package io.spring.testingweb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+	
+	@Autowired
+	private HomeController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
